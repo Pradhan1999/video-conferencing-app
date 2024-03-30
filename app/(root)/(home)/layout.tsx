277abdd265a73bf21c1d9507bb5676392d0fb599
Deepak <pradhan171219@gmail.com>
2024-03-30 21:26:03 +0530

    update layout.tsx

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,13 +1,19 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="relative">
-      <div className="bg-orange-100">NAVBAR</div>
+      <div>
+        <Navbar />
+      </div>
       <div className="flex">
-        <div className="bg-blue-100">SIDEBAR</div>
+        <div>
+          <Sidebar />
+        </div>
         <section className="flex flex-1 min-h-screen flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14 bg-yellow-100">
-          {children}
+          <div className="w-full">{children}</div>
         </section>
       </div>
     </main>
